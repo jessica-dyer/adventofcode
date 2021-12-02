07e4fde7a5fee4a77973ff9047195b42aaaeda07
jessica-dyer <jessica.dyer@gmail.com>
2021-12-02 14:30:10 -0800

    Updated Submarine class with new instructions related to aim.

--- puzzles/puzzle_3.py
+++ puzzles/puzzle_3.py
@@ -1,11 +1,14 @@
 with open('submarine_input_1.txt') as f:
     lines = f.readlines()
 
+test_input = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']
+
 
 class Submarine:
     def __init__(self):
         self.currentHorizontalPosition = 0
         self.currentDepth = 0
+        self.aim = 0
 
     def move(self, input_str):
         current_instructions = self.parse_instructions(input_str)
@@ -13,10 +16,11 @@ class Submarine:
         current_amount = current_instructions[1]
         if current_direction == 'forward':
             self.currentHorizontalPosition = self.currentHorizontalPosition + current_amount
+            self.currentDepth = self.currentDepth + (self.aim * current_amount)
         elif current_direction == 'down':
-            self.currentDepth = self.currentDepth + current_amount
+            self.aim = self.aim + current_amount
         elif current_direction == 'up':
-            self.currentDepth = self.currentDepth - current_amount
+            self.aim = self.aim - current_amount
 
     def parse_instructions(self, input_str):
         split_str = input_str.split(' ')
