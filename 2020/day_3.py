088bf745ea3453dc10986032c9e0e68ff5349882
jessica-dyer <jessica.dyer@gmail.com>
2021-12-03 16:28:09 -0800

    Created a map class and a traveller class. My MountainWoman travelled through the forest!

--- 2020/day_3.py
+++ 2020/day_3.py
@@ -1,13 +1,49 @@
 with open('day_3_input.txt') as f:
-    lines = f.readlines()
+    map_of_land = []
+    for readline in f:
+        line_strip = readline.strip()
+        map_of_land.append(line_strip)
+test_map = ['..##.......', '#...#...#..', ".#....#..#.", "..#.#...#.#", ".#...##..#.", "..#.##.....", ".#.#.#....#",
+            ".#........#", "#.##...#...", "#...##....#", ".#..#...#.#"]
 
-class Tobagen:
-    def __init__(self, right, down):
-        self.right = 0
-        self.down = 0
 
-    def move(self, right, down):
-        pass
+class LandMap:
+    def __init__(self, map_of_land):
+        self.map_of_land = []
 
-    def isTree(self):
-        pass 
+        for topo in map_of_land:
+            temp_topo = (list(topo))
+            self.map_of_land.append(temp_topo)
+
+
+class Traveller:
+    def __init__(self, map_of_land: LandMap):
+        self.horizontal_position = 0
+        self.vertical_position = 0
+        self.map_object = LandMap(map_of_land)
+        self.horizontal_position_is_max = False
+
+    def isOnTree(self):
+        return self.map_object.map_of_land[self.vertical_position][self.horizontal_position] == '#'
+
+    def move(self, right: int, down: int):
+        max_horizontal_index = len(self.map_object.map_of_land[0])-1
+        for num in range(right):
+            if self.horizontal_position == max_horizontal_index:
+                self.horizontal_position = -1
+            self.horizontal_position += 1
+        self.vertical_position += down
+
+    def reset(self, horizontal_position, vertical_position):
+        self.horizontal_position = horizontal_position
+        self.vertical_position = vertical_position
+
+
+mountainWoman = Traveller(map_of_land)
+boolean_array = []
+while mountainWoman.vertical_position < len(mountainWoman.map_object.map_of_land)-1:
+    mountainWoman.move(1, 2)
+    bool = mountainWoman.isOnTree()
+    boolean_array.append(bool)
+
+answer = sum(boolean_array) # 80*162*77*83*37
