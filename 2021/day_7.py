522d44a971d45c6d1f691074e48872f22e5d70d2
jessica-dyer <jessica.dyer@gmail.com>
2021-12-07 07:16:16 -0800

    Completed day 7 part 2 with a fuel lookup table.

--- 2021/day_7.py
+++ 2021/day_7.py
@@ -1,4 +1,4 @@
-with open('day_7_input_test.txt') as f:
+with open('day_7_input.txt') as f:
     array_of_crab_positions = []
     for line in f:
         array_of_crab_positions.append(line)
@@ -9,16 +9,20 @@ array_of_crab_positions = list(map(int, array_of_crab_positions))
 cumulative_moves_for_each_index = []
 max_crab_position = max(array_of_crab_positions)
 
+fuel_lookup_table = []
+for n in range(max_crab_position + 10):
+    fuel_used = 0
+    for num in range(n + 1):
+        fuel_used += num
+    fuel_lookup_table.append(fuel_used)
+
 for num in range(max_crab_position + 1):
     current_cummulative_moves = 0
     for position in array_of_crab_positions:
         fuel_used = 0
-        distance = abs(num-position) + 1
-        for n in range(distance):
-            fuel_used += n
+        distance = abs(num-position)
+        fuel_used = fuel_lookup_table[distance]
         current_cummulative_moves += fuel_used
     cumulative_moves_for_each_index.append(current_cummulative_moves)
-    if num % 2 == 2:
-        print('Working.')
 
 answer = cumulative_moves_for_each_index.index(min(cumulative_moves_for_each_index))
