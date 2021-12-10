4b67e0e6fdc9bbf4c01c4d4342f9f68dc1af992f
Jessica Dyer <jessica.dyer@gmail.com>
2021-12-09 22:54:49 -0800

    Trying to solve part 2 day 9.

--- 2021/day_9.py
+++ 2021/day_9.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 with open('day_9_input_test.txt') as f:
     array_of_heights = []
     for line in f:
@@ -8,15 +10,14 @@ with open('day_9_input_test.txt') as f:
             current_array_of_heights.append(item)
         array_of_heights.append(current_array_of_heights)
 
-
-# noinspection PyStatementEffect,PyUnreachableCode
 class HeightMap:
     def __init__(self, array_of_array_of_heights):
         self.master_map = array_of_array_of_heights
         self.max_index_rows = len(self.master_map)
         self.max_columns = len(self.master_map[0])
 
-    # It's okay to pass x and y coordinates out of bounds, in that case, None is returned.
+        # It's okay to pass x and y coordinates out of bounds, in that case, None is returned.
+
     def return_value_at_coordinates(self, x_coord, y_coord):
         if x_coord < 0 or x_coord >= self.max_columns:
             return None
@@ -24,14 +25,19 @@ class HeightMap:
             return None
         return self.master_map[y_coord][x_coord]
 
+
+class HeightMapApp:
+    def __init__(self, array_of_array_of_heights):
+        self.height_map = HeightMap(array_of_array_of_heights)
+
     def space_is_lowpoint(self, x_coord, y_coord):
         adjacent_values = []
-        current_value = self.return_value_at_coordinates(x_coord, y_coord)
+        current_value = self.height_map.return_value_at_coordinates(x_coord, y_coord)
         # Check up, down, left, right
-        adjacent_values.append(self.return_value_at_coordinates(x_coord, y_coord - 1))
-        adjacent_values.append(self.return_value_at_coordinates(x_coord, y_coord + 1))
-        adjacent_values.append(self.return_value_at_coordinates(x_coord - 1, y_coord))
-        adjacent_values.append(self.return_value_at_coordinates(x_coord + 1, y_coord))
+        adjacent_values.append(self.height_map.return_value_at_coordinates(x_coord, y_coord - 1))
+        adjacent_values.append(self.height_map.return_value_at_coordinates(x_coord, y_coord + 1))
+        adjacent_values.append(self.height_map.return_value_at_coordinates(x_coord - 1, y_coord))
+        adjacent_values.append(self.height_map.return_value_at_coordinates(x_coord + 1, y_coord))
         adjacent_values = list(filter(lambda x: x is not None, adjacent_values))
         for num in adjacent_values:
             if num < current_value:
@@ -40,16 +46,70 @@ class HeightMap:
 
     def calculate_risk_level(self, x_coord, y_coord):
         if self.space_is_lowpoint(x_coord, y_coord):
-            return self.master_map[y_coord][x_coord] + 1
+            return self.height_map.return_value_at_coordinates(x_coord, y_coord) + 1
 
 
+class MapPoint(Enum):
+    NINE = 9
+    BASIN_PART = "B"
+    COUNTED = "C"
+    UNKNOWN = 0
 
 
+class BasinAreaMap:
+    def __init__(self, height_map_objct):
+        self.height_map = height_map_objct
+        self.row_length = self.height_map.max_columns
+        self.column_height = self.height_map.max_index_rows
+        self.basin_map = [self.create_empty_row() for x in range(self.column_height)]
+        self.build_basin_map()
+
+    def create_empty_row(self):
+        return [MapPoint.UNKNOWN for x in range(self.row_length)]
+
+    def build_basin_map(self):
+        for row in range(self.column_height):
+            for column in range(self.row_length):
+                if self.height_map.return_value_at_coordinates(column, row) == 9:
+                    self.basin_map[row][column] = MapPoint.NINE
+                else:
+                    self.basin_map[row][column] = MapPoint.BASIN_PART
+
+    # Returns an array of ints
+    def get_list_of_basin_areas(self):
+        list_of_basin_areas = []
+        for row in range(self.column_height):
+            for column in range(self.row_length):
+                if self.basin_map[row][column] == MapPoint.BASIN_PART:
+                    area_of_basin = self.count_of_basin_parts(row, column)
+                    list_of_basin_areas.append(area_of_basin)
+        return list_of_basin_areas
+
+    # Returns an integer that is the count of all contiguous basin parts
+    # THIS IS A DESTRUCTIVE FUNCTION
+    def count_of_basin_parts(self, row, column):
+        if row < 0 or row >= self.column_height:
+            return 0
+        if column < 0 or column >= self.row_length:
+            return 0
+        if self.basin_map[row][column] != MapPoint.BASIN_PART:
+            return 0
+        # Now we know we're at a basin part
+        counter = 0
+        counter += 1
+        self.basin_map[row][column] = MapPoint.COUNTED
+        counter += self.count_of_basin_parts(row + 1, column)
+        counter += self.count_of_basin_parts(row - 1, column)
+        counter += self.count_of_basin_parts(row, column + 1)
+        counter += self.count_of_basin_parts(row, column - 1)
+        return counter
 
 
-sum_of_risk_level = 0
-map = HeightMap(array_of_heights)
-for num in range(map.max_index_rows):
-    for n in range(map.max_columns):
-        if map.space_is_lowpoint(n, num):
-            sum_of_risk_level += map.calculate_risk_level(n, num)
+poo = HeightMap(array_of_heights)
+foo = BasinAreaMap(poo)
+# sum_of_risk_level = 0
+# map = HeightMapApp(array_of_heights)
+# for num in range(map.max_index_rows):
+#     for n in range(map.max_columns):
+#         if map.space_is_lowpoint(n, num):
+#             sum_of_risk_level += map.calculate_risk_level(n, num)
