bf5bfdfba8f44a2ae663244f42eac94f1facac94
jessica-dyer <jessica.dyer@gmail.com>
2021-12-10 13:51:58 -0800

    Updated day 8 part 2.

--- 2021/day_8.py
+++ 2021/day_8.py
@@ -1,35 +1,38 @@
-with open('day_8_input.txt') as f:
+from itertools import *
+with open('day_8_input_test.txt') as f:
     segment_displays = []
     for line in f:
         line = line.strip()
         segment_displays.append(line)
 
-output_values = []
-for item in segment_displays:
-    split_item = item.split(' | ')
-    output_values.append(split_item[1])
-
-counter = 0
-for item in output_values:
-    current_words = item.split(' ')
-    for word in current_words:
-        if len(word) == 2 or len(word) == 4 or len(word) == 3 or len(word) == 7:
-            counter += 1
+# output_values = []
+# for item in segment_displays:
+#     split_item = item.split(' | ')
+#     output_values.append(split_item[1])
+#
+# counter = 0
+# for item in output_values:
+#     current_words = item.split(' ')
+#     for word in current_words:
+#         if len(word) == 2 or len(word) == 4 or len(word) == 3 or len(word) == 7:
+#             counter += 1
 
 # PART 2
+t = 0
+for k in segment_displays:
+    a, b = k.split(" | ")
+    do = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+    req = set(do)
+    for x in permutations("abcdefg"):
+        m = {i: j for i, j in zip(x, "abcdefg")}
+        r = {"".join(sorted(map(m.get, q))) for q in a.split()}
+        if r == req:
+            b = ["".join(sorted(map(m.get, q))) for q in b.split()]
+            b = "".join(str(do.index(q)) for q in b)
+            t += int(b)
 
+print(t)
 
-class SevenSegmentDisplay:
-    def __init__(self, board_as_array_of_arrays):
-        self.display = board_as_array_of_arrays
-
-    @classmethod
-    def build_display(cls, array_of_seven_letters: list):
-        display = []
-
-        for num in range()
-
-        return SevenSegmentDisplay()
 
 
 
