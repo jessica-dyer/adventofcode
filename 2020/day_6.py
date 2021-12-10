ad949dc275fa997c7bb43d1c028db6e47a8d1b12
jessica-dyer <jessica.dyer@gmail.com>
2021-12-10 14:44:37 -0800

    Completed day 6 2020.

--- 2020/day_6.py
+++ 2020/day_6.py
@@ -15,7 +15,7 @@
 #     unique_items = len(set(item))
 #     total_yes += unique_items
 
-with open('day_6_input_test.txt') as f:
+with open('day_6_input.txt') as f:
     array_of_answers = []
     answers_from_all_people_in_group = []
     string_accumulator = ''
@@ -34,3 +34,19 @@ with open('day_6_input_test.txt') as f:
     answers_from_all_people_in_group.append(string_accumulator)
     array_of_answers.append(answers_from_all_people_in_group)
 
+total_score = 0
+for group in array_of_answers:
+    individual_answers = group[0].split(',')
+    number_people_in_group = len(individual_answers)
+    group_counter = {}
+    for item in individual_answers:
+        for character in item:
+            if character in group_counter:
+                group_counter[character] += 1
+            else:
+                group_counter[character] = 1
+        for key in group_counter:
+            if group_counter[key] == number_people_in_group:
+                total_score += 1
+
+print(total_score)
