d65c7147015e056c235d01978452bc0f6ae0d991
Jessica Dyer <jessica.dyer@gmail.com>
2021-12-13 08:01:34 -0800

    Working on 2020 day 7, the bags.

--- 2020/day_7.py
+++ 2020/day_7.py
@@ -4,10 +4,10 @@ with open('day_7_input_test.py') as f:
         line = line.strip()
         array_of_rules.append(line)
 
-class Bag:
+class BagRule:
     def __init__(self, parsed_string):
         self.bag_color = parsed_string[0]
-        self.rules = parsed_string[1]
+        self.can_contain = parsed_string[1]
 
     @classmethod
     def build_bag(cls, rules_as_string):
@@ -22,22 +22,64 @@ class Bag:
                 rules[color] = value
             parsed_string.append(rules)
 
-        return Bag(parsed_string)
-bags = []
+        return BagRule(parsed_string)
+bag_rules = []
 for rules_as_string in array_of_rules:
-    bag = Bag.build_bag(rules_as_string)
-    bags.append(bag)
+    bag = BagRule.build_bag(rules_as_string)
+    bag_rules.append(bag)
 
-direct_carry = []
-for bag in bags:
-    if 'shiny gold' in bag.rules:
-        direct_carry.append(bag.bag_color)
+# array of arrays each inner array is the list of colors that could hold the previous colors
+wrap_layers = []
+# start with the color you want to bring with you: 'shiny gold'
+# loop over all bag rules and determine which can contain 'shiny gold' return bag_color if can contain
+# end up with an array of new colors
+# take that array and reduce it to only new colors
+# if there are no new colors in that array, break out
+# add that as a new layer to wrap layers
 
-secondary_carry = []
-for color in direct_carry:
-    for bag in bags:
-        if color in bag.rules:
-            secondary_carry.append(bag.bag_color)
-# secondary_carry = set(secondary_carry)
-#
-# print(len(direct_carry) + len(secondary_carry))
+
+# function that takes one color
+# returns a list of colors that can contain it
+def array_of_bag_colors_that_hold_bag_color(single_bag_color):
+#      TODO: use the bag_rules array in here
+    pass
+
+# takes: array of colors
+# returns: array of colors
+def all_bag_colors_that_can_hold_any_of_these(array_of_colors):
+#     TODO: use a loop and call array_of_bag_colors_that_hold_bag_color()
+    pass
+
+# takes: array of colors
+# returns: array of colors
+def reduce_to_new_colors(array_of_colors):
+# for each color given, check if that color is anywhere in our previous history of wrap layers
+# return an array that only contains that are not
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def return_array_of_bags_that_hold_bag(array_of_colors):
+#     current_bags = array_of_colors
+#     for color in current_bags:
+#         if color in bag.rules:
+#             current_bags.append(bag.bag_color)
+#     current_bags = list(set(current_bags))
+#     return current_bags
+
