2d8604ebfb5f09fbb2053d2ef81bdffb6a19afc9
jessica-dyer <jessica.dyer@gmail.com>
2021-12-16 18:45:44 -0800

    Finished the damn bags. Recursion is my nemesis.

--- 2020/day_7.py
+++ 2020/day_7.py
@@ -1,4 +1,4 @@
-with open('day_7_input_test.py') as f:
+with open('day_7_input.py') as f:
     array_of_rules = []
     for line in f:
         line = line.strip()
@@ -110,23 +110,45 @@ def search_for_remaining_bag_colors(array_of_bag_rules: BagRule):
     return array_of_new_bag_rules
 
 
-foo = search_for_one_bag_color('shiny gold')
-# boo = search_for_remaining_bag_colors(foo)
-counter = 0
-for rule in foo:
-    for key in rule.can_contain:
-        current_num_of_bags = rule.can_contain[key]
-        new_array = search_for_one_bag_color(key)
-        for bag_rule in new_array:
-            counter += current_num_of_bags
-            for key in bag_rule.can_contain:
-                num_bags_contained = current_num_of_bags * bag_rule.can_contain[key]
-                counter += num_bags_contained
-
-
-
-
 # takes a bag color, multiplier
 # returns the sum of all nested bags below that (inclusive of current bag) i.e. if this is called with vibrant plum and 2, (5+6)*2 + 2 (original bags)
 
-def number_of_bags(bag_color: string, multiplier):
+def find_number_of_bags(bag_color: str, multiplier):
+    counter = 0
+    for bag_rule in bag_rules:
+        if bag_rule.bag_color == bag_color:
+            for key in bag_rule.can_contain:
+                num_bags_contained = multiplier * find_number_of_bags(key, bag_rule.can_contain[key])
+                counter += num_bags_contained
+            break
+    counter += multiplier
+    return counter
+
+# print(find_number_of_bags('shiny gold', 1)-1)
+
+# PART 2 REDUX
+def number_of_bags_within(outer_bag_color: str):
+    outer_bags_rule = get_rule_for_bag_color(outer_bag_color)
+    total_inner_bags = 0
+    for inner_bag_color in outer_bags_rule.can_contain:
+        number_of_inner_bag = outer_bags_rule.can_contain[inner_bag_color]
+        bags_inside_each_inner_bag = number_of_bags_within(inner_bag_color)
+        total_inner_bags += number_of_inner_bag + (bags_inside_each_inner_bag * number_of_inner_bag)
+    return total_inner_bags
+
+# Returns a bag rule object
+def get_rule_for_bag_color(bag_color: str):
+    for bag_rule in bag_rules:
+        if bag_rule.bag_color == bag_color:
+            return bag_rule
+    return None
+
+# dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags
+# for each inner bag, find out how many bags that color contains and then multiply it by the number of inner bag
+# add all of those together and return that
+
+print('faded blue bags contain 0 other bags: ' , number_of_bags_within('faded blue'))
+print('dark olive bags contain 7 other bags: ' , number_of_bags_within('dark olive'))
+print('shiny gold bags contain 32 other bags: ' , number_of_bags_within('shiny gold'))
+
+
