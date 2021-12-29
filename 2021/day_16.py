878f6eef26e1c9ead51cfcd8ce826c6877bbe220
Jessica Dyer <jessica.dyer@gmail.com>
2021-12-29 09:45:10 -0800

    Working on day 16.

--- 2021/day_16.py
+++ 2021/day_16.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-input = 'D2FE28'
+input = '38006F45291200'
 
 
 def hex_to_binary(input: str):
@@ -15,17 +15,18 @@ binary_input = hex_to_binary(input)
 
 
 class AbstractPacket(ABC):
-    def __init__(self, version, type):
+    def __init__(self, version, type, original_bit_length):
         self.version = version
         self.type = type
+        self.original_bit_length = original_bit_length
 
     @abstractmethod
     def sum_of_version_and_all_sub_packet_versions(self):
         pass
 
 class LiteralPacket(AbstractPacket):
-    def __init__(self, version, literal_value):
-        super().__init__(version, 4)
+    def __init__(self, version, literal_value, original_bit_length):
+        super().__init__(version, 4, original_bit_length)
         self.literal_value = literal_value
 
     def sum_of_version_and_all_sub_packet_versions(self):
@@ -33,8 +34,8 @@ class LiteralPacket(AbstractPacket):
 
 
 class OperatorPacket(AbstractPacket):
-    def __init__(self, version: int, type: int, array_of_subpackets: list):
-        super().__init__(version, type)
+    def __init__(self, version: int, type: int, array_of_subpackets: list, original_bit_length):
+        super().__init__(version, type, original_bit_length)
         self.array_of_subpackets = array_of_subpackets
 
     def sum_of_version_and_all_sub_packet_versions(self):
@@ -59,25 +60,41 @@ class PacketFactory:
 
     # return type is an instance of AbstractPacket
     def build_literal_packet(self, version, type, remaining_binary_string):
-        literal_value = 42
-        # TODO: come up with a literal value using remaining_binary_string
-        new_packet = LiteralPacket(version, literal_value)
+        literal_value = 42  # should be 2021 for input D2FE28
+        original_bit_length = 6  # should be 21 for input D2FE28
+        final_binary = ''
+        for index in range(0, len(remaining_binary_string), 5):
+            current_five_char = remaining_binary_string[index:index + 5]
+            if current_five_char[0] == '0':
+                original_bit_length += len(current_five_char)
+                final_binary += current_five_char[1:5]
+                literal_value = binary_to_decimal(final_binary)
+                break
+            final_binary += current_five_char[1:5]
+            original_bit_length += len(current_five_char)
+        new_packet = LiteralPacket(version, literal_value, original_bit_length)
         return new_packet
 
     # return type is an instance of AbstractPacket
     def build_operator_packet(self, version, type, remaining_binary_string):
         sub_packets = []
+        length_type_id = remaining_binary_string[0]
+        remaining_binary_string = remaining_binary_string[1:len(remaining_binary_string)]
+        original_bit_length = 42
         # TODO: build subpackets
-        new_packet = OperatorPacket(version, type, sub_packets)
+        if length_type_id == '0':
+            length_of_sub_packets = binary_to_decimal(remaining_binary_string[0:15])
+        elif:
+            length_of_sub_packets = binary_to_decimal(remaining_binary_string[0:11])
+        new_packet = OperatorPacket(version, type, sub_packets, original_bit_length)
         return new_packet
 
     def get_sum_of_versions(self):
         sum = self.root_packet.sum_of_version_and_all_sub_packet_versions()
         return sum
 
+
 # Takes: a string of binary
-
-
 # Returns: a decimal representation of binary base 2 array
 def binary_to_decimal(binary_string: str):
     base_2_array = []
@@ -108,18 +125,6 @@ def get_type(binary_string: str):
     return decimal
 
 
-def get_literal_type_four(binary_string: str):
-    string_length = len(binary_string)
-    literal = binary_string[6:string_length]
-    final_string = ''
-    for index in range(0, len(literal), 5):
-        current_five_char = literal[index:index + 5]
-        if len(current_five_char) < 5:
-            final_string = binary_to_decimal(final_string)
-            return final_string
-        final_string += current_five_char[1:5]
-
-
 def get_length_of_sub_packets(binary_string: str):
     length_type_id = binary_string[6]
     if length_type_id == '0':
@@ -128,16 +133,6 @@ def get_length_of_sub_packets(binary_string: str):
         bits = binary_string[7:18]
     dec = binary_to_decimal(bits)
     return dec
-
-
-def run_decode(binary_string: str):
-    current_version = get_version(binary_string)
-    current_type = get_type(binary_string)
-
-    if current_type == 4:
-        return get_literal_type_four(binary_string)
-    else:
-        get_length_of_sub_packets(binary_string)
 
 
 my_packet_factory = PacketFactory(binary_input)
