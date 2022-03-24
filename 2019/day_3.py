fd34150e06251927a7cb16d203ebe00c86345581
Jessica Dyer <jessica.dyer@gmail.com>
2022-03-23 17:53:13 -0700

    working on day 3

--- 2019/day_3.py
+++ 2019/day_3.py
@@ -1,18 +1,69 @@
-
+import re
 def contstructInput(inputFileName: str):
+  splits = re.compile("([a-zA-Z]+)([0-9]+)")
   with open(inputFileName) as f:
-    listOfStrings = []
+    listOfInstructions = []
     for line in f:
+      current_line = []
       line = line.strip()
       line = line.split(",")
-      listOfStrings.append(line)
-  return listOfStrings
+      for item in line:
+        if item != '':
+          result = splits.match(item).groups()
+        current_line.append(result)
+      listOfInstructions.append(current_line)
+  return listOfInstructions
 
 allInstructions = contstructInput("2019/day_3_input.txt")
 
+class Coordinates:
+  def __init__(self, x, y):
+      self.x = x
+      self.y = y
+
+  def __str__(self) -> str:
+      return self.x, self.y
+
 class CircuitBoard:
   def __init__(self, listOfWires) -> None:
-      pass
+      self.wires = listOfWires
+      self.board = []
+
+  def findMaxBoardDims(self):
+    max_x = 0
+    max_y = 0
+    for wire in self.wires:
+      wire_max_x = 0
+      wire_max_y = 0
+      for pair in wire.instructions:
+        current_direction = pair[0]
+        current_amount = int(pair[1])
+        if current_direction == 'R':
+          wire_max_x += current_amount
+        elif current_direction == 'L':
+          wire_max_x -= current_amount
+        elif current_direction == "U":
+          wire_max_y += current_amount
+        elif current_direction == "D":
+          wire_max_y -= current_amount
+      if wire_max_x > max_x:
+        max_x = wire_max_x
+      if wire_max_y > max_y:
+        max_y = wire_max_y
+    return (max_x, max_y)
+
+  def generateBoard(self):
+    max_dimensions = self.findMaxBoardDims()
+    board = []
+    for y in range(max_dimensions[1]):
+      one_row = []
+      for x in range(max_dimensions[0]):
+        coordinate = Coordinates(x, y)
+        one_row.append(coordinate)
+      board.append(one_row)
+    self.board = board
+    return
+
 
   def calculateManhattanDisctanceAtCross():
     pass
@@ -29,3 +80,16 @@ class Wire:
 
   def isCrossed():
     pass
+
+
+listOfWires = []
+for instruction in allInstructions:
+  newWire = Wire(instruction)
+  listOfWires.append(newWire)
+
+circuitBoard = CircuitBoard(listOfWires)
+print(circuitBoard.wires)
+
+print(listOfWires)
+print(circuitBoard.generateBoard())
+circuitBoard.board
