7008224f01d526cb186c337ef00f9f3687e55d24
Jessica Dyer <jessica.dyer_intern@leveltenenergy.com>
2022-10-24 09:05:54 -0700

    finished day 8

--- 2019/day_8.py
+++ 2019/day_8.py
@@ -1,6 +1,8 @@
+import cv2
+import imageio
 import numpy as np
-from aocd import get_data
-from dataclasses import dataclass
+
+from stitch import stitch
 
 with open('2019/day_8_input.txt', 'r') as data:
     data = np.fromiter(data.read().strip(), int).reshape((-1, 6, 25))
@@ -11,3 +13,11 @@ print((fewest_zeros == 1).sum() * (fewest_zeros == 2).sum()) # Part 1
 row, col = np.mgrid[:6, :25]
 decoded = data[(data != 2).argmax(axis=0), row, col]
 print(*map(''.join, decoded.astype(str)), sep='\n') # Part 2
+
+decoded = data[0]
+# for i, layer in enumerate(data):
+#    decoded = np.where(decoded != 2, decoded, layer)
+#    cv2.imwrite(f'frames/{i:03d}.png',
+#                cv2.resize(np.where(decoded != 1, 0, 255), (250, 60), interpolation=cv2.INTER_NEAREST))
+
+# stitch(filename='transmission')
