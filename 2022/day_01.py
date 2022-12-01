cfc946ecee387e2b39d8625dae070a2b3f0c6fec
Jessica Dyer <jessica.dyer_intern@leveltenenergy.com>
2022-12-01 10:13:59 -0800

    finished day 1

--- 2022/day_01.py
+++ 2022/day_01.py
@@ -1,18 +1,30 @@
 import aoc_helper
 
 RAW = aoc_helper.day(1)
-print(RAW)
 
 def parse_raw():
-    ...
+    parsed_data = RAW.splitlines()
+    dictionary = {}
+    elf_number = 0
+    total_calories = 0
+    for item in parsed_data: 
+        if item != '': 
+            total_calories += int(item)
+        else: 
+            dictionary[elf_number] = total_calories
+            elf_number += 1
+            total_calories = 0
+    return dictionary
+        
 
 DATA = parse_raw()
 
 def part_one():
-    ...
+    return max(DATA.values())
 
 def part_two():
-    ...
+    list_of_values = sorted(DATA.values())
+    return sum(list_of_values[-3:])
 
 aoc_helper.submit(1, part_one)
 aoc_helper.submit(1, part_two)
