f55965b926bcc5cb5f2ed9302c86f5ecb95c9df7
Jessica Dyer <jessica.dyer_intern@leveltenenergy.com>
2022-12-02 16:11:35 -0800

    more refactors

--- 2022/day_02.py
+++ 2022/day_02.py
@@ -4,10 +4,13 @@ import aoc_lube.utils
 RAW = aoc_helper.day(2)
 POINTS_FOR_DRAW = 3
 POINTS_FOR_WIN = 6
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
 
 points = dict(
-    A=0, B=1, C=2, 
-    X=0, Y=1, Z=2
+    A=ROCK, B=PAPER, C=SCISSORS, 
+    X=ROCK, Y=PAPER, Z=SCISSORS
 )
 
 GAMES = [
@@ -16,38 +19,38 @@ GAMES = [
 ]
 
 def i_won(a: int, b: int)->bool: 
-    if b == 2 and a == 1: 
+    if b == SCISSORS and a == PAPER: 
         return True
-    return True if b == 1 and a == 0 else b == 0 and a == 2
+    return True if b == PAPER and a == ROCK else b == ROCK and a == SCISSORS
 
 def is_draw(a: int, b: int)-> bool: 
     return a == b
 
 def score(a: int, b: int) -> int: 
     if i_won(a, b): 
-        return POINTS_FOR_WIN + b + 1
+        return POINTS_FOR_WIN + b
     elif is_draw(a, b): 
-        return POINTS_FOR_DRAW + b + 1
-    return b + 1
+        return POINTS_FOR_DRAW + b
+    return b
 
 def return_losing_int(a: int) -> int: 
     pick = a - 1
-    return 2 if pick < 0 else pick
+    return SCISSORS if pick < ROCK else pick
 
 def return_winning_int(a: int) -> int: 
     pick = a + 1
-    return 0 if pick > 2 else pick
+    return ROCK if pick > SCISSORS else pick
 
 def return_int_for_draw(a: int) -> int: 
     return a 
 
 def score_two(a: int, b: int) -> int: 
-    if b == 0: 
-        return return_losing_int(a=a) + 1
-    elif b == 1: 
-        return return_int_for_draw(a=a) + POINTS_FOR_DRAW + 1
+    if b == ROCK: 
+        return return_losing_int(a=a)
+    elif b == PAPER: 
+        return return_int_for_draw(a=a) + POINTS_FOR_DRAW
     else: 
-        return return_winning_int(a=a) + POINTS_FOR_WIN + 1
+        return return_winning_int(a=a) + POINTS_FOR_WIN
 
 def part_one():
     return sum(score(a, b) for a, b in GAMES)
@@ -55,6 +58,5 @@ def part_one():
 def part_two():
     return sum(score_two(a, b) for a, b in GAMES)
 
-print(part_two())
 aoc_helper.submit(2, part_one)
 aoc_helper.submit(2, part_two)
