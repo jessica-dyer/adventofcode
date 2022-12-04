b1de2cbdc71bffa4e438da8b6f4f8a826277cf77
Jessica Dyer <jessica.dyer_intern@leveltenenergy.com>
2022-12-03 21:44:41 -0800

    day 4

--- 2022/day_04.py
+++ 2022/day_04.py
@@ -1,22 +1,18 @@
 import aoc_helper
+from aoc_helper.utils import extract_ints
 
-RAW = aoc_helper.day(4)
-print(RAW)
-
-
-def parse_raw():
-    ...
-
-
-DATA = parse_raw()
+RAW = aoc_helper.day(4).split("\n")
+PAIRS = [tuple(extract_ints(str(pairs.split("-")))) for pairs in RAW]
 
 
 def part_one():
-    ...
+    return sum(a <= x and b >= y or x <= a and y >= b for a, b, x, y in PAIRS)
 
 
 def part_two():
-    ...
+    return sum(
+        bool(set(range(a, b + 1)) & set(range(x, y + 1))) for a, b, x, y in PAIRS
+    )
 
 
 aoc_helper.submit(4, part_one)
