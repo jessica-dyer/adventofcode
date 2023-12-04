a7e084317a4fbbca658d154264aa0f1b142b86bf
Jessica Dyer <jessica.dyer@leveltenenergy.com>
2023-12-04 09:45:07 -0800

    added day 5

---
+++ 2023/5/main.py
@@ -0,0 +1,14 @@
+from aockit import get_input
+
+
+def part1():
+    data = get_input(2023, 5)
+
+
+def part2():
+    data = get_input(2023, 5)
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
