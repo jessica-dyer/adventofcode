49ab0ad81c528bd631a41880a16cfa543d048c64
Jessica Dyer <jessica.dyer@leveltenenergy.com>
2023-12-07 09:00:51 -0800

    added day 8

---
+++ 2023/8/main.py
@@ -0,0 +1,18 @@
+from aockit import get_input
+
+PROD_DATA = get_input(2023, 8)
+PROD = False
+TEST_DATA = """"""
+INPUT_DATA = PROD_DATA if PROD else TEST_DATA
+
+def part1():
+    data = get_input(2023, 8)
+
+
+def part2():
+    data = get_input(2023, 8)
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
