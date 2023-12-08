dff631c533efe280e3d2e74cbbc9332dc5b64d9b
Jessica Dyer <jessica.dyer@leveltenenergy.com>
2023-12-08 08:27:11 -0800

    added alternative solution

--- 2023/8/main.py
+++ 2023/8/main.py
@@ -1,5 +1,6 @@
 from aockit import get_input
 from math import lcm
+from itertools import cycle
 
 PROD_DATA = get_input(2023, 8)
 PROD = True
@@ -15,18 +16,27 @@ ZZZ = (ZZZ, ZZZ)"""
 
 INPUT_DATA = PROD_DATA if PROD else TEST_DATA
 
-INTSTRUCTIONS = INPUT_DATA.splitlines().pop(0)
-DATA = {
+INSTRUCTIONS = INPUT_DATA.splitlines().pop(0)
+NETWORK = {
     k: {"L": v.split(", ")[0][1:], "R": v.split(", ")[1][:-1]}
     for k, v in (item.split(" = ") for item in INPUT_DATA.splitlines()[2:])
 }
 
 
+def traverse(start, end_condition):
+    """Alternative solution, shorter."""
+    current = start
+    for i, dir in enumerate(cycle(INSTRUCTIONS)):
+        current = NETWORK[current][dir]
+        if end_condition(current):
+            return i + 1
+
+
 def find_number_of_steps(start: str):
     index = 0
     while True:
-        current_instruction = INTSTRUCTIONS[index % len(INTSTRUCTIONS)]
-        return_value = DATA[start].get(current_instruction)
+        current_instruction = INSTRUCTIONS[index % len(INSTRUCTIONS)]
+        return_value = NETWORK[start].get(current_instruction)
 
         if return_value and return_value.endswith("Z"):
             return index + 1
@@ -41,8 +51,8 @@ def part1():
     index = 0
 
     while True:
-        current_instruction = INTSTRUCTIONS[index % len(INTSTRUCTIONS)]
-        return_value = DATA[start].get(current_instruction)
+        current_instruction = INSTRUCTIONS[index % len(INSTRUCTIONS)]
+        return_value = NETWORK[start].get(current_instruction)
 
         if return_value == end:
             return index + 1
@@ -51,9 +61,13 @@ def part1():
         index += 1
 
 
+def part1_redux():
+    return traverse("AAA", lambda node: node == "ZZZ")
+
+
 def part2():
     start_values = []
-    for k in DATA:
+    for k in NETWORK:
         if k.endswith("A"):
             start_values.append(k)
     list_num_of_steps = []
@@ -63,6 +77,17 @@ def part2():
     return lcm(*list_num_of_steps)
 
 
+def part2_redux():
+    dist = (
+        traverse(node, lambda node: node.endswith("Z"))
+        for node in NETWORK
+        if node.endswith("A")
+    )
+    return lcm(*dist)
+
+
 if __name__ == "__main__":
     print(part1())
+    print(part1_redux())
     print(part2())
+    print(part2_redux())
