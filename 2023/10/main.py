49bb53f04142f1784e9ce72b72514f5a4282fdbc
Jessica Dyer <jessica.dyer@leveltenenergy.com>
2023-12-11 09:14:37 -0800

    ugh these are too difficult

--- 2023/10/main.py
+++ 2023/10/main.py
@@ -1,20 +1,77 @@
 from aockit import get_input
+from typing import NamedTuple
+import networkx as nx
 
 PROD_DATA = get_input(2023, 10)
-PROD = False
-TEST_DATA = """"""
+PROD = True
+TEST_DATA = """-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF"""
 
 INPUT_DATA = PROD_DATA if PROD else TEST_DATA
 
+DATA = [list(line) for line in INPUT_DATA.splitlines()]
+
+class Point(NamedTuple):
+    y: int
+    x: int
+
+    def __add__(self, other):
+        return Point(self.y + other.y, self.x + other.x)
+
+DIRS = [N, E, S, W] = [Point(-1, 0), Point(0, 1), Point(1, 0), Point(0, -1)]
+Δs = {"|": (N, S), "J": (N, W), "L": (N, E), "7": (S, W), "F": (S, E), "-": (W, E)}
+
+def parse_raw():
+    grid = [list(line) for line in INPUT_DATA.splitlines()]
+    yield grid
+
+    edges = set()
+    for y, line in enumerate(grid):
+        for x, char in enumerate(line):
+            pos = Point(y, x)
+            if char in Δs:
+                Δ1, Δ2 = Δs[char]
+                edges.add((pos, pos + Δ1))
+                edges.add((pos, pos + Δ2))
+            elif char == "S":
+                start = pos
+                for Δ in DIRS:
+                    edges.add((pos, pos + Δ))
+
+    for pipe, (Δ1, Δ2) in Δs.items():
+        if (start + Δ1, start) in edges and (start + Δ2, start) in edges:
+            grid[start.y][start.x] = pipe
+            break
+
+    H = nx.Graph(((u, v) for u, v in edges if (v, u) in edges))
+    for component in nx.connected_components(H):
+        if start in component:
+            yield component
+            return
+
+
+GRID, PIPES = parse_raw()
+
 
 def part1():
-    ...
+    return len(PIPES) // 2
 
 
 def part2():
-    ...
+    ntiles = 0
+    for y, line in enumerate(GRID):
+        inside = False
+        for x, pipe in enumerate(line):
+            if (y, x) not in PIPES:
+                ntiles += inside
+            elif pipe in "|F7":
+                inside = not inside
+    return ntiles
 
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    print(part1())
+    print(part2())
---
+++ 2023/11/main.py
@@ -0,0 +1,55 @@
+from aockit import get_input
+import numpy as np
+from itertools import combinations
+
+PROD_DATA = get_input(2023, 11)
+PROD = True
+TEST_DATA = """...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."""
+
+INPUT_DATA = PROD_DATA if PROD else TEST_DATA
+
+DATA = INPUT_DATA.splitlines()
+
+
+def parse_raw():
+    yield np.array([y for y, line in enumerate(DATA) for char in line if char == "#"])
+    yield np.array([x for x in range(len(DATA[0])) for line in DATA if line[x] == "#"])
+
+
+YS, XS = parse_raw()
+
+
+def expand(axis, n):
+    diffs = np.diff(axis, prepend=0) - 1
+    diffs[diffs < 0] = 0
+    return axis + np.cumsum(diffs) * n
+
+
+def expand_universe(n):
+    return sum(
+        abs(a - b)
+        for axis in (expand(YS, n), expand(XS, n))
+        for a, b in combinations(axis, 2)
+    )
+
+
+def part1():
+    return expand_universe(1)
+
+
+def part2():
+    return expand_universe(999_999)
+
+
+if __name__ == "__main__":
+    print(part1())
+    print(part2())
